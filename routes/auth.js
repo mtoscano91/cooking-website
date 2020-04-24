309792866a73b68aafb887de0982f807b159b36c
Manu <manueltoscano91@hotmail.com>
2020-04-24 17:20:14 +0200

    my first commit

---
+++ routes/auth.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const User = require("../models/User");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true,
+  })
+);
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
+
+router.post("/signup", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate email and password" });
+    return;
+  }
+
+  if (!ValidateEmail(email)) {
+    res.render("auth/signup", {
+      message: "You have entered an invalid email address!",
+    });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+    });
+
+    newUser
+      .save()
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((err) => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
