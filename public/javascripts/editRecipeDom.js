dfcb1602801c007dc13bd68e45bd5951cfbebc7c
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-04-28 17:33:13 +0200

    edit recipe almost done

--- public/javascripts/editRecipeDom.js
+++ public/javascripts/editRecipeDom.js
@@ -1,4 +1,4 @@
-console.log(recipeObj);
+// console.log(recipeObj);
 
 function fixTagInputsCheckbox() {
   let allTagInputs = document.querySelectorAll(".tag-input-checkbox");
@@ -11,3 +11,42 @@ function fixTagInputsCheckbox() {
   }
 }
 fixTagInputsCheckbox();
+
+function fixdifficulty() {
+  let allDificulty = document.querySelectorAll(".tag-input-difficulty");
+  // console.log("boombb", allDificulty.length);
+  for (let i = 0; i < allDificulty.length; i++) {
+    if (recipeObj.difficulty == allDificulty[i].value) {
+      allDificulty[i].selected = true;
+    }
+  }
+}
+fixdifficulty();
+
+function fixservings() {
+  let allServings = document.querySelectorAll(".tag-input-servings");
+  // console.log("boombb", allDificulty.length);
+  for (let i = 0; i < allServings.length; i++) {
+    if (recipeObj.servings == allServings[i].value) {
+      allServings[i].selected = true;
+    }
+  }
+}
+fixservings();
+
+function fixmeasure() {
+  let allMeasure = document.querySelectorAll("#measure");
+  console.log(allMeasure);
+  const allSelectArr = Array.from(allMeasure);
+  console.log(allSelectArr);
+  const addingToArrTheIndexes = allSelectArr.map((selectArr, index) => {
+    // console.log(el);
+    return Array.from(selectArr).map((option) => {
+      if (option.innerText === recipeObj.ingredients[index].measure) {
+        option.selected = true;
+      }
+      return option;
+    });
+  });
+}
+fixmeasure();
