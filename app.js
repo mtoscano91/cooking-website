047d441341eb34fb6abc4a10e43208cd46f58fe1
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-04-29 18:13:27 +0200

    try

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/new-project", {
     useNewUrlParser: true,
   })
+
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
